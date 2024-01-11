2c6b9d24f107689fd52c322f4afa39f7cadccb0a
cruelladevil <dev.timetravel@gmail.com>
2024-01-11 12:24:12 +0900

    feat: 타이포그래피 및 컬러시스템을 객체로 변경

---
+++ src/styles/typography.ts
@@ -0,0 +1,26 @@
+export const typography = {
+  headline: {
+    fontSize: '32px',
+    fontWeight: '500',
+    letterSpacing: '-0.02em',
+    lineHeight: '1.3em',
+  },
+
+  body1: {
+    fontSize: '16px',
+    fontWeight: '500',
+    letterSpacing: '-0.01em',
+  },
+
+  body2: {
+    fontSize: '16px',
+    fontWeight: '400',
+    letterSpacing: '-0.01em',
+  },
+
+  smallBody: {
+    fontSize: '14px',
+    fontWeight: '400',
+    letterSpacing: '-0.01em',
+  },
+};
